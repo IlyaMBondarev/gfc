99d7c14abb8e62708f2593636815d8a3dc496b28
IlyaMBondarev <60775995+IlyaMBondarev@users.noreply.github.com>
2021-01-22 16:02:45 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -197,8 +197,8 @@ var _loop2 = function _loop2(index) {
 
                 for (var j = 0; j < items.length; j++) {
                     if (j !== indexOfActiveItem) {
-                        items[j].querySelector('.item__title_hor').classList.add('item__title-visible');
-                        items[j].querySelector('.item__title_ver').classList.remove('item__title-visible');
+                        items[j].querySelector('.item__title_hor').classList.remove('item__title-visible');
+                        items[j].querySelector('.item__title_ver').classList.add('item__title-visible');
                     }
                 }
             }
